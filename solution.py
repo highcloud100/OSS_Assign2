5bbdcd85f31c774b12f36a716cf836b30d40f21d
highcloud_acer <highcloud100@inha.edu>
2022-11-20 19:59:20 +0900

    typo

--- solution.py
+++ solution.py
@@ -66,30 +66,32 @@ def svm_train_test(x_train, x_test, y_train, y_test):
     return acc, prec, recall
 
 def print_performances(acc, prec, recall):
-    print("Accuracy: ", acc)
-    print("Precision: ", prec)
-    print("Recall: ", recall)
+	#Do not modify this function!
+	print ("Accuracy: ", acc)
+	print ("Precision: ", prec)
+	print ("Recall: ", recall)
 
 if __name__ == '__main__':
-    #Do not modify the main script!
-    data_path = sys.argv[1]
-    data_df = load_dataset(data_path)
-    n_feats, n_class0, n_class1 = dataset_stat(data_df)
-    print ("Number 0f features: ", n_feats)
-    print ("Number Of c1ass 0 data entries: ", n_class0)
-    print ("Number Of class 1 data entries: ", n_class1)
+	#Do not modify the main script!
+	data_path = sys.argv[1]
+	data_df = load_dataset(data_path)
 
-    print ("\nSplitting the dataset with the test size of ", float(sys.argv[2]))
-    x_train, x_test, y_train, y_test = split_dataset(data_df, float(sys.argv[2]))
+	n_feats, n_class0, n_class1 = dataset_stat(data_df)
+	print ("Number of features: ", n_feats)
+	print ("Number of class 0 data entries: ", n_class0)
+	print ("Number of class 1 data entries: ", n_class1)
 
-    acc, prec, recall = decision_tree_train_test(x_train, x_test, y_train, y_test)
-    print("\nDecsision Tree Performances")
-    print_performances(acc, prec, recall)
+	print ("\nSplitting the dataset with the test size of ", float(sys.argv[2]))
+	x_train, x_test, y_train, y_test = split_dataset(data_df, float(sys.argv[2]))
 
-    acc, prec, recall = random_forest_train_test(x_train, x_test, y_train, y_test)
-    print("\nRandom Forest Performances")
-    print_performances(acc, prec, recall)
+	acc, prec, recall = decision_tree_train_test(x_train, x_test, y_train, y_test)
+	print ("\nDecision Tree Performances")
+	print_performances(acc, prec, recall)
 
-    acc, prec, recall = svm_train_test(x_train, x_test, y_train, y_test)
-    print("\nSVM Performances")
-    print_performances(acc, prec, recall)
+	acc, prec, recall = random_forest_train_test(x_train, x_test, y_train, y_test)
+	print ("\nRandom Forest Performances")
+	print_performances(acc, prec, recall)
+
+	acc, prec, recall = svm_train_test(x_train, x_test, y_train, y_test)
+	print ("\nSVM Performances")
+	print_performances(acc, prec, recall)
